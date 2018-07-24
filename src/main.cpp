d4a8f47746d3113a0e3fdb60b786d6dcbb3c40ee
NovakovicV <novakovic.vladimir@alumni.ubc.ca>
2018-07-23 21:43:56 -0700

    It turns out that the pins were actually reversed for forwards and reverse. The motor value of 220 should get us up the second gap if we have a head start. The angle does not need to be perfect, but if it is too far off, the robot will fall into the gap.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <HBRIDGE.h>
 
 //create tracks
-HBRIDGE left_motor = HBRIDGE(PB0, PA1);
-HBRIDGE right_motor = HBRIDGE(PA7,PA3);
+HBRIDGE left_motor = HBRIDGE(PA1, PB0);
+HBRIDGE right_motor = HBRIDGE(PA3,PA7);
 
 //The decleration of the HBRIDGE objects here has the correct pin layout
 //for each motor
@@ -12,22 +12,12 @@ void setup()
   Serial.begin(9600); // Start Serial
   left_motor.init();
   right_motor.init();
+  delay(5000);
+  int pos = 220;
+  left_motor.run(pos);
+  right_motor.run(pos);
 }
 
 void loop()
 {
-  int pos = 0;
-  for (pos = -255; pos <= 255; pos += 1)
-  { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    left_motor.run(pos);
-    right_motor.run(pos); // tell servo to go to position in variable 'pos'
-    delay(15);          // waits 15ms for the servo to reach the position
-  }
-  for (pos = 255; -255 >= 0; pos -= 1)
-  {                     // goes from 180 degrees to 0 degrees
-    left_motor.run(pos);
-    right_motor.run(pos); // tell servo to go to position in variable 'pos'
-    delay(15);          // waits 15ms for the servo to reach the position
-  }
 }
