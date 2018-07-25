5f5d1c348636e48291c3759c10ef8275494c30b3
Vladislav Pomogaev <comsa44@gmail.com>
2018-07-24 21:25:17 -0700

    changed closed limit on arm

--- lib/armControl/armControl.cpp
+++ lib/armControl/armControl.cpp
@@ -17,7 +17,7 @@ Control class for arm motion. The arm has 3 positions:
 #define DEADBAND 100
 #define STOP 1473
 #define GRABBER_OPEN 160
-#define GRABBER_CLOSE 5
+#define GRABBER_CLOSE 30
 #define UP_LIMIT 1300
 #define DOWN_LIMIT 2950
 
